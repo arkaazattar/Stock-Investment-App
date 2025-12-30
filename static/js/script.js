c8bc9341f394e7420edd47a100fd9fdf6beff15b
Arkaaz <arkaazattar@gmail.com>
2025-12-30 01:46:49 -0500

    js modularity for flask
    Co-authored-by: Triet Duong <triet.m.duong@gmail.com>

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+    const form = document.getElementById("form")
+
+    form.addEventListener("submit", async function(event){
+        event.preventDefault();
+        // console.log("Button Pressed") 
+    })
