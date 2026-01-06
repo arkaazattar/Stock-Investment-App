58a256b0fa210456906bd8233d663c5ba18d9d8e
arkaazattar <arkaazattar@gmail.com>
2026-01-05 22:21:00 -0500

    fixed file layout, need auth branch to merge to main to see proper routing

---
+++ app/static/js/getstarted.js
@@ -0,0 +1,14 @@
+const signup = document.getElementById("signup");
+const login = document.getElementById("login");
+
+signup.addEventListener("click", function(event){
+    event.preventDefault();
+    window.location.href="/signup"
+    console.log("button pressed")
+})
+
+login.addEventListener("click", function(event){
+    event.preventDefault();
+    window.location.href="/login"
+    console.log("button pressed")
+})
